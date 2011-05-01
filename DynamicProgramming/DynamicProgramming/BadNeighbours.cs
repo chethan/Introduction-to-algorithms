a5a95e17d2b8ae45addad3c3b5f1ad1c93e13de6
Chethan <yourschethu@gmail.com>
2011-05-01 18:40:53 +0530

    one more problem solved using dynamic programming

---
+++ DynamicProgramming/DynamicProgramming/BadNeighbours.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicProgramming
+{
+    public static class BadNeighbours
+    {
+        public static int MaxDonations(this List<int> donations)
+        {
+            int maxDonationsIncludingFirst = MaxDonationsLinear(donations.TakeWhile((i, index) => index+1 != donations.Count).ToList());
+            int maxDonationsIncludingLast = MaxDonationsLinear(donations.SkipWhile((i, index) => index  == 0).ToList());
+            return maxDonationsIncludingFirst > maxDonationsIncludingLast ? maxDonationsIncludingFirst:maxDonationsIncludingLast;
+        }
+
+        private static int MaxDonationsLinear(List<int> donations)
+        {
+            var maxDonationList = new List<int>();
+            for (int i = 0; i < donations.Count; i++)
+            {
+                maxDonationList.Add(donations[i]);
+                if(i==1 && maxDonationList[0] > maxDonationList[1])  maxDonationList[1] = maxDonationList[0];
+                else if(i>1)
+                {
+                    int nMinus2 = i - 2 < 0 ? 0 : maxDonationList[i - 2] + donations[i];
+                    int nMinus3 = i - 3 < 0 ? 0 : maxDonationList[i - 3] + donations[i];
+                    maxDonationList[i] = nMinus2 > nMinus3 ? nMinus2 : nMinus3;
+                }
+            }
+            return maxDonationList.Last();
+        }
+    }
+}
--- DynamicProgramming/DynamicProgramming/Program.cs
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -7,11 +7,10 @@ namespace DynamicProgramming
     {
         public static void Main(string[] args)
         {
-            var coinsProblem = new CoinsProblem(1,3,5);
-            Console.WriteLine(coinsProblem.MinimumNumberOfCoins(11));
+            Console.WriteLine(new CoinsProblem(1,3,5).MinimumNumberOfCoins(11));
             Console.WriteLine(new List<int> { 1, 3, 5, 2, 7, 0, 11 }.LongestIncreasingSubsequence());
             Console.WriteLine(new List<int> { 1, 3, 5, 2, 7, 0, 11 }.TogglingSubsequence());
-
+            Console.WriteLine(new List<int> {10,3,2,5,7,8 }.MaxDonations());
             Console.ReadLine();
         }
     }

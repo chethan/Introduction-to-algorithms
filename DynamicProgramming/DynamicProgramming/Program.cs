e2b4be3dab397349d9f3e0d4c35a7dcd1ddced45
Chethan <yourschethu@gmail.com>
2011-05-17 12:32:29 +0530

    Subset sum using dynamic programming

--- DynamicProgramming/DynamicProgramming/Program.cs
+++ DynamicProgramming/DynamicProgramming/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 
 namespace DynamicProgramming
 {
@@ -9,13 +7,13 @@ namespace DynamicProgramming
     {
         public static void Main(string[] args)
         {
-            var numbers = new int[,]{{3,0,0,0},{7,4,0,0},{2,4,6,0},{8,5,9,3}};
-            Console.WriteLine(new TriangleNumbers(numbers).MaximumSum());
-            Console.WriteLine(new CoinsProblem(1, 3, 5).MinimumNumberOfCoins(11));
-            Console.WriteLine(new List<int> {1, 3, 5, 2, 7, 0, 11}.LongestIncreasingSubsequence());
-            Console.WriteLine(new List<int> {1, 3, 5, 2, 7, 0, 11}.TogglingSubsequence());
-            Console.WriteLine(new List<int> {10, 3, 2, 5, 7, 8}.MaxDonations());
-            Console.WriteLine("BDCABA".ToList().LeastCommonSubsequence(new List<char>("ABCBDAB")));
+//            Console.WriteLine(new TriangleNumbers(new[,]{{3,0,0,0},{7,4,0,0},{2,4,6,0},{8,5,9,3}}).MaximumSum());
+//            Console.WriteLine(new CoinsProblem(1, 3, 5).MinimumNumberOfCoins(11));
+//            Console.WriteLine(new List<int> {1, 3, 5, 2, 7, 0, 11}.LongestIncreasingSubsequence());
+//            Console.WriteLine(new List<int> {1, 3, 5, 2, 7, 0, 11}.TogglingSubsequence());
+//            Console.WriteLine(new List<int> {10, 3, 2, 5, 7, 8}.MaxDonations());
+//            Console.WriteLine("BDCABA".ToList().LeastCommonSubsequence(new List<char>("ABCBDAB")));
+            Console.WriteLine(new List<int> {802,421,143,-302,137,316,150,-611,-466,-42,-195,-295}.GetSubSetsForSum(0));
             Console.ReadLine();
         }
 
---
+++ DynamicProgramming/DynamicProgramming/SubsetSum.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicProgramming
+{
+    public static class SubsetSum
+    {
+        public static List<int> GetSubSetsForSum(this List<int> list,int sum)
+        {
+            int positiveSum = list.Sum(i => i>0?i:0);
+            int negativeSum = list.Sum(i => i>0?0:i);
+            var subsetSums = new bool[list.Count,positiveSum-negativeSum+1];
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j <=(positiveSum-negativeSum); j++)
+                {
+                    subsetSums[i, j] = false; 
+                    if (list[i] == j+negativeSum) subsetSums[i, j] = true;
+                    else if(i>0)
+                    {
+                        if (subsetSums[i - 1, j] || (j - list[i] > 0 && j - list[i] <= positiveSum-negativeSum && subsetSums[i - 1, j - list[i]]))
+                            subsetSums[i, j] = true;
+                    }
+                }
+            }
+
+            bool isSumPossible = sum >= negativeSum && sum <= positiveSum && subsetSums[list.Count - 1, sum-negativeSum];
+            return GenerateSubset(list, sum, subsetSums, negativeSum, isSumPossible);
+        }
+
+        private static List<int> GenerateSubset(IList<int> list, int sum, bool[,] subsetSums, int negativeSum, bool isSumPossible)
+        {
+            var subset = new List<int>();
+            if (!isSumPossible) return subset;
+            for (int i = list.Count - 1, j = sum - negativeSum; i >= 0; i--)
+            {
+                if (subsetSums[i, j] && (i==0 || !subsetSums[i-1, j]))
+                {
+                    subset.Add(list[i]);
+                    j = j - list[i];
+                }
+            }
+            return subset;
+        }
+    }
+}
